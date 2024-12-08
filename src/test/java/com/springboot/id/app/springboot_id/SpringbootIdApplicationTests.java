b777deb1129943eb32a255cf998583f4a57dbb71
cuter97 <aguscastets@gmail.com>
2024-12-08 19:37:10 -0300

    config files java

---
+++ src/test/java/com/springboot/id/app/springboot_id/SpringbootIdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.id.app.springboot_id;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootIdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
